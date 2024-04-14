5e21aa4084f807bc16db9694ddc2f96688a78e73
Osaro Iyoha <saro.iyoha@gmail.com>
2024-04-14 12:55:37 +0100

    update app.css

--- components/AccordionItem.jsx
+++ components/AccordionItem.jsx
@@ -3,12 +3,10 @@ import iconPlus from '../assets/images/icon-plus.svg'
 import iconMinus from '../assets/images/icon-minus.svg'
 const AccordionItem = ({title,info}) => {
   const [showInfo, setShowInfo] = useState(false);
-  const softRedColor = '#fa5757';
-  const softBlueColor = '#5267DF';
   return <>
     <div className={`accordion-item ${showInfo && "accordion-item--active"}`} onClick={()=>setShowInfo(!showInfo)}>
       <div className="accordion-item--header">
-          <h4>{title}</h4>
+          <h2>{title}</h2>
           <button className="accordion-item--header__btn" aria-label="accordion dropdown toggle" >
             <img src={`${showInfo?iconMinus:iconPlus}`} alt="" />
           </button>
